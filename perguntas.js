d0d4771e4aec8edd27f75df4fdba5bdcb4e9087e
Luca243153 <luca.bresolin@escola.pr.gov.br>
2025-08-14 16:35:29 -0300

    final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Quem é conhecida como "rainha do pop"',
+    'Madonna'
+)
+
+criaCartao(
+    'Música',
+    'Qual cantora ficou famosa com "Bad romance"',
+    'Lady Gaga'
+)
+
+criaCartao(
+    'Música',
+    'Qual o nome do estilo musical brasileiro criado no Rio de Janeiro e famoso por batidas dançantes?',
+    'funk carioca'
+)
+
+criaCartao(
+    'Música',
+    'Quem é conhecida como "Rei do pop"',
+    'Michael jackson'
+)
